2aad28980552c92bd734adcea199e10ac2fd93cf
Anna Geller <anna.m.geller@gmail.com>
2023-08-04 13:20:33 +0200

    Update FileTransform.java

--- plugin-script-jython/src/main/java/io/kestra/plugin/scripts/jython/FileTransform.java
+++ plugin-script-jython/src/main/java/io/kestra/plugin/scripts/jython/FileTransform.java
@@ -17,6 +17,40 @@ import io.kestra.core.runners.RunContext;
 @Plugin(
     examples = {
         @Example(
+            full = true,
+            title = "Extract data from an API, add a column and store it as a downloadable CSV file",
+            code = """     
+id: etl-api-to-csv
+namespace: dev
+
+tasks:
+  - id: download
+    type: io.kestra.plugin.fs.http.Download
+    uri: https://gorest.co.in/public/v2/users
+
+  - id: ionToJSON
+    type: "io.kestra.plugin.serdes.json.JsonReader"
+    from: "{{outputs.download.uri}}"
+    newLine: false
+
+  - id: writeJSON
+    type: io.kestra.plugin.serdes.json.JsonWriter
+    from: "{{outputs.ionToJSON.uri}}"
+
+  - id: addColumn
+    type: io.kestra.plugin.scripts.jython.FileTransform
+    from: "{{outputs.writeJSON.uri}}"
+    script: |
+      from datetime import datetime
+      logger.info('row: {}', row)
+      row['inserted_at'] = datetime.utcnow()
+
+  - id: csv
+    type: io.kestra.plugin.serdes.csv.CsvWriter
+    from: "{{outputs.addColumn.uri}}"
+"""
+        ),           
+        @Example(
             title = "Transform with file from internal storage",
             code = {
                 "from: \"{{ outputs['avro-to-gcs'] }}\"",
