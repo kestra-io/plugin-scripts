0a701addc0dbe5ac33e89ae575d45c37918322be
Benoit <benoit@kestra.io>
2023-08-09 16:32:28 +0200

    [feature] add malloy CLI task and corresponding artifacts

---
+++ plugin-script-malloy/src/main/java/io/kestra/plugin/scripts/malloy/CLI.java
@@ -0,0 +1,80 @@
+package io.kestra.plugin.scripts.malloy;
+
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.runners.RunContext;
+import io.kestra.plugin.scripts.exec.AbstractExecScript;
+import io.kestra.plugin.scripts.exec.scripts.models.DockerOptions;
+import io.kestra.plugin.scripts.exec.scripts.models.ScriptOutput;
+import io.kestra.plugin.scripts.exec.scripts.services.ScriptService;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "Execute one or more Julia commands from the Command Line Interface."
+)
+@Plugin(examples = {
+    @Example(
+        full = true,
+        title = "Install package, create a julia script and execute it",
+        code = """
+            id: "local-files"
+            namespace: "io.kestra.tests"
+
+            tasks:
+              - id: workingDir
+                type: io.kestra.core.tasks.flows.WorkingDirectory
+                tasks:
+                - id: inputFiles
+                  type: io.kestra.core.tasks.storages.LocalFiles
+                  inputs:
+                    main.js: |
+                      const colors = require("colors");
+                      console.log(colors.red("Hello"));
+                - id: bash
+                  type: io.kestra.plugin.scripts.julia.Commands
+                  beforeCommands:
+                    - npm install colors
+                  commands:
+                    - julia main.jl
+            """
+    )
+})
+public class CLI extends AbstractExecScript {
+    @Schema(
+        title = "Docker options when using the `DOCKER` runner"
+    )
+    @PluginProperty
+    @Builder.Default
+    protected DockerOptions docker = DockerOptions.builder()
+        .image("malloy")
+        .build();
+
+    @Schema(
+        title = "The commands to run"
+    )
+    @PluginProperty(dynamic = true)
+    protected List<String> commands;
+
+    @Override
+    public ScriptOutput run(RunContext runContext) throws Exception {
+        List<String> commandsArgs = ScriptService.scriptCommands(
+            this.interpreter,
+            this.beforeCommands,
+            this.commands
+        );
+
+        return this.commands(runContext)
+            .withCommands(commandsArgs)
+            .run();
+    }
+}
---
+++ plugin-script-malloy/src/main/java/io/kestra/plugin/scripts/malloy/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for running Malloy CLI.",
+    categories = { PluginSubGroup.PluginCategory.SCRIPT }
+)
+package io.kestra.plugin.scripts.malloy;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
