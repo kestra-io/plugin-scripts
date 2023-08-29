013605d0deeaacf16dde9470d9413565ac9c76cb
Loïc Mathieu <loikeseke@gmail.com>
2023-08-29 10:28:01 +0200

    fix(plugin-script-node): rename Node package

--- plugin-script-node/src/main/java/io/kestra/core/tasks/scripts/package-info.java
+++ plugin-script-node/src/main/java/io/kestra/core/tasks/scripts/package-info.java
@@ -1,4 +1,4 @@
 @PluginSubGroup(categories = PluginSubGroup.PluginCategory.SCRIPT)
-package io.kestra.core.tasks.script;
+package io.kestra.core.tasks.scripts;
 
 import io.kestra.core.models.annotations.PluginSubGroup;
