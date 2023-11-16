01b8a6b89897ad416c35b1179fd83e9f1fde9a6d
Ludovic DEHON <tchiot.ludo@gmail.com>
2023-11-16 22:11:32 +0100

    feat(ruby): introduce ruby plugins

---
+++ plugin-script-ruby/src/main/java/io/kestra/plugin/scripts/ruby/Commands.java
@@ -0,0 +1,96 @@
+package io.kestra.plugin.scripts.ruby;
+
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.runners.RunContext;
+import io.kestra.plugin.scripts.exec.AbstractExecScript;
+import io.kestra.plugin.scripts.exec.scripts.models.DockerOptions;
+import io.kestra.plugin.scripts.exec.scripts.models.ScriptOutput;
+import io.kestra.plugin.scripts.exec.scripts.services.ScriptService;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(
+    title = "Execute R from the Command Line Interface."
+)
+@Plugin(examples = {
+    @Example(
+        full = true,
+        title = "Create a Ruby script, install a package and execute it",
+        code = """
+            id: "local-files"
+            namespace: "io.kestra.tests"
+
+            tasks:
+              - id: workingDir
+                type: io.kestra.core.tasks.flows.WorkingDirectory
+                tasks:
+                - id: inputFiles
+                  type: io.kestra.core.tasks.storages.LocalFiles
+                  inputs:
+                    main.R: |
+                      library(lubridate)
+                      ymd("20100604");
+                      mdy("06-04-2011");
+                      dmy("04/06/2012")
+                - id: bash
+                  type: io.kestra.plugin.scripts.ruby.Commands
+                  beforeCommands:
+                    - Rscript -e 'install.packages("lubridate")'
+                  commands:
+                    - Rscript main.R
+            """
+    )
+})
+public class Commands extends AbstractExecScript {
+    private static final String DEFAULT_IMAGE = "ruby";
+
+    @Schema(
+        title = "Docker options when using the `DOCKER` runner",
+        defaultValue = "{image=" + DEFAULT_IMAGE + ", pullPolicy=ALWAYS}"
+    )
+    @PluginProperty
+    @Builder.Default
+    protected DockerOptions docker = DockerOptions.builder().build();
+
+    @Schema(
+        title = "The commands to run"
+    )
+    @PluginProperty(dynamic = true)
+    @NotEmpty
+    protected List<String> commands;
+
+    @Override
+    protected DockerOptions injectDefaults(DockerOptions original) {
+        var builder = original.toBuilder();
+        if (original.getImage() == null) {
+            builder.image(DEFAULT_IMAGE);
+        }
+
+        return builder.build();
+    }
+
+    @Override
+    public ScriptOutput run(RunContext runContext) throws Exception {
+        List<String> commandsArgs = ScriptService.scriptCommands(
+            this.interpreter,
+            this.beforeCommands,
+            this.commands
+        );
+
+        return this.commands(runContext)
+            .withCommands(commandsArgs)
+            .run();
+    }
+}
---
+++ plugin-script-ruby/src/test/java/io/kestra/plugin/scripts/ruby/CommandsTest.java
@@ -0,0 +1,69 @@
+package io.kestra.plugin.scripts.ruby;
+
+import com.google.common.collect.ImmutableMap;
+import io.kestra.core.models.executions.LogEntry;
+import io.kestra.core.queues.QueueFactoryInterface;
+import io.kestra.core.queues.QueueInterface;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.runners.RunContextFactory;
+import io.kestra.core.storages.StorageInterface;
+import io.kestra.core.utils.TestsUtils;
+import io.kestra.plugin.scripts.exec.scripts.models.ScriptOutput;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@MicronautTest
+class CommandsTest {
+    @Inject
+    RunContextFactory runContextFactory;
+
+    @Inject
+    StorageInterface storageInterface;
+
+    @Inject
+    @Named(QueueFactoryInterface.WORKERTASKLOG_NAMED)
+    private QueueInterface<LogEntry> logQueue;
+
+    @Test
+    void task() throws Exception {
+        List<LogEntry> logs = new ArrayList<>();
+        logQueue.receive(l -> logs.add(l.getLeft()));
+
+        URI put = storageInterface.put(
+            null,
+            new URI("/file/storage/get.yml"),
+            IOUtils.toInputStream(
+                "puts \"Hello World!\"\n" +
+                    "STDERR.puts \"done\"",
+                StandardCharsets.UTF_8
+            )
+        );
+
+        Commands bash = Commands.builder()
+            .id("unit-test")
+            .type(Script.class.getName())
+            .commands(List.of("ruby " + put.toString()))
+            .build();
+
+        RunContext runContext = TestsUtils.mockRunContext(runContextFactory, bash, ImmutableMap.of());
+        ScriptOutput run = bash.run(runContext);
+
+        assertThat(run.getExitCode(), is(0));
+        assertThat(run.getStdOutLineCount(), is(1));
+        assertThat(run.getStdErrLineCount(), is(1));
+
+        TestsUtils.awaitLog(logs, log -> log.getMessage() != null && log.getMessage().contains("Hello World"));
+        assertThat(logs.stream().filter(logEntry -> logEntry.getMessage() != null && logEntry.getMessage().contains("done")).count(), is(1L));
+    }
+}
