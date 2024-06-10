a6ccb8d0bb1149cb4b3d57a5da053f7d20caae95
Shruti Mantri <shruti1810@gmail.com>
2024-06-10 21:11:07 +0530

    feat(docs): add groovy filetransform example using jackson library

--- plugin-script-groovy/src/main/java/io/kestra/plugin/scripts/groovy/FileTransform.java
+++ plugin-script-groovy/src/main/java/io/kestra/plugin/scripts/groovy/FileTransform.java
@@ -53,6 +53,32 @@ import io.kestra.core.runners.RunContext;
                 "  }"
             }
         ),
+        @Example(
+            title = "JSON transformations using jackson library",
+            full = true,
+            code = """
+id: json_transform_using_jackson
+namespace: dev
+
+tasks:
+  - id: "file_transform"
+    type: "io.kestra.plugin.scripts.groovy.FileTransform"
+    from: "[{\"name\":\"John Doe\", \"age\":99, \"embedded\":{\"foo\":\"bar\"}}]"
+    script: |
+      import com.fasterxml.jackson.*
+
+      def mapper = new databind.ObjectMapper();
+      def jsonStr = mapper.writeValueAsString(row);
+      logger.info('input in json str: {}', jsonStr)
+
+      def typeRef = new core.type.TypeReference<HashMap<String,Object>>() {};
+
+      data = mapper.readValue(jsonStr, typeRef);
+  
+      logger.info('json object: {}', data);
+      logger.info('embedded field: {}', data.embedded.foo)
+"""
+        )
     }
 )
 public class FileTransform extends io.kestra.plugin.scripts.jvm.FileTransform {
