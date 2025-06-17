3ea427c9ec49165deb5a4fcbaea9faa2a8e4347c
Malaydewangan09 <malaydewangan310@gmail.com>
2025-06-17 23:34:15 +0530

    feat(scripts): add support for Perl Scripts and Commands tasks

---
+++ plugin-script-perl/src/main/java/io/kestra/plugin/scripts/perl/Commands.java
@@ -0,0 +1,56 @@
+package io.kestra.plugin.scripts.perl;
+
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.models.tasks.runners.TargetOS;
+import io.kestra.core.runners.RunContext;
+import io.kestra.plugin.scripts.exec.AbstractExecScript;
+import io.kestra.plugin.scripts.exec.scripts.models.ScriptOutput;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import java.util.List;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema(title = "Execute Perl commands from the CLI.")
+@Plugin(examples = {
+    @Example(
+        full = true,
+        title = "Execute a simple Perl command.",
+        code = """
+            id: perl_commands
+            namespace: company.team
+            tasks:
+              - id: perl
+                type: io.kestra.plugin.scripts.perl.Commands
+                commands:
+                  - perl -e 'print "Hello from Kestra!\\n";'
+            """
+    )
+})
+public class Commands extends AbstractExecScript {
+    private static final String DEFAULT_IMAGE = "perl";
+
+    @Builder.Default
+    protected Property<String> containerImage = Property.ofValue(DEFAULT_IMAGE);
+
+    @Schema(title = "The commands to run.")
+    protected Property<List<String>> commands;
+
+    @Override
+    public ScriptOutput run(RunContext runContext) throws Exception {
+        TargetOS os = runContext.render(this.targetOS).as(TargetOS.class).orElse(null);
+
+        return this.commands(runContext)
+            .withInterpreter(this.interpreter)
+            .withBeforeCommands(this.beforeCommands)
+            .withCommands(this.commands)
+            .withTargetOS(os)
+            .run();
+    }
+}
