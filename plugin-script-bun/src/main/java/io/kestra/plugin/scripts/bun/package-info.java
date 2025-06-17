a4c2a836e2d8a68125114e470a89a59d09c2945d
Malay Dewangan <66718045+Malaydewangan09@users.noreply.github.com>
2025-06-17 22:19:16 +0530

    feat(scripts): add support for Bun Scripts and Commands tasks (#260)

---
+++ plugin-script-bun/src/main/java/io/kestra/plugin/scripts/bun/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for running Bun script.",
+    categories = { PluginSubGroup.PluginCategory.SCRIPT }
+)
+package io.kestra.plugin.scripts.bun;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
---
+++ plugin-script-bun/src/test/java/io/kestra/plugins/scripts/bun/ScriptTest.java
@@ -0,0 +1,56 @@
+package io.kestra.plugins.scripts.bun;
+
+import com.google.common.collect.ImmutableMap;
+import io.kestra.core.junit.annotations.KestraTest;
+import io.kestra.core.models.executions.LogEntry;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.queues.QueueFactoryInterface;
+import io.kestra.core.queues.QueueInterface;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.runners.RunContextFactory;
+import io.kestra.core.utils.TestsUtils;
+import io.kestra.plugin.scripts.bun.Script;
+import io.kestra.plugin.scripts.exec.scripts.models.ScriptOutput;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@KestraTest
+public class ScriptTest {
+
+    @Inject
+    RunContextFactory runContextFactory;
+
+    @Inject
+    @Named(QueueFactoryInterface.WORKERTASKLOG_NAMED)
+    private QueueInterface<LogEntry> logQueue;
+
+    @Test
+    void script() throws Exception {
+        List<LogEntry> logs = new ArrayList<>();
+        Flux<LogEntry> receive = TestsUtils.receive(logQueue, l -> logs.add(l.getLeft()));
+
+        Script bunScript = Script.builder()
+            .id("unit-test")
+            .type(Script.class.getName())
+            .script(Property.ofValue("console.log(\"Kestra is amazing!\");"))
+            .build();
+
+        RunContext runContext = TestsUtils.mockRunContext(runContextFactory, bunScript, ImmutableMap.of());
+        ScriptOutput run = bunScript.run(runContext);
+
+        assertThat(run.getExitCode(), is(0));
+        assertThat(run.getStdOutLineCount(), is(1));
+
+        TestsUtils.awaitLog(logs, log -> log.getMessage() != null && log.getMessage().contains("Kestra is amazing!"));
+        receive.blockLast();
+        assertThat(logs.stream().anyMatch(log -> log.getMessage() != null && log.getMessage().contains("Kestra is amazing!")), is(true));
+    }
+}
