22cbec189926fe098dddb251e6803eca6c249e43
Malay Dewangan <66718045+Malaydewangan09@users.noreply.github.com>
2025-06-23 00:34:48 +0530

    feat(scripts): add support for Deno Scripts and Commands tasks (#263)

---
+++ plugin-script-deno/src/test/java/io/kestra/plugins/scripts/deno/CommandsTest.java
@@ -0,0 +1,56 @@
+package io.kestra.plugins.scripts.deno;
+
+import com.google.common.collect.ImmutableMap;
+import io.kestra.core.junit.annotations.KestraTest;
+import io.kestra.core.models.executions.LogEntry;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.queues.QueueFactoryInterface;
+import io.kestra.core.queues.QueueInterface;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.runners.RunContextFactory;
+import io.kestra.core.utils.IdUtils;
+import io.kestra.core.utils.TestsUtils;
+import io.kestra.plugin.scripts.deno.Commands;
+import io.kestra.plugin.scripts.exec.scripts.models.ScriptOutput;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@KestraTest
+public class CommandsTest {
+
+    @Inject
+    RunContextFactory runContextFactory;
+
+    @Inject
+    @Named(QueueFactoryInterface.WORKERTASKLOG_NAMED)
+    private QueueInterface<LogEntry> logQueue;
+
+    @Test
+    void task() throws Exception {
+        List<LogEntry> logs = new ArrayList<>();
+        Flux<LogEntry> receive = TestsUtils.receive(logQueue, l -> logs.add(l.getLeft()));
+
+        Commands commands = Commands.builder()
+            .id(IdUtils.create())
+            .type(Commands.class.getName())
+            .commands(Property.ofValue(List.of("deno eval \"console.log('Hello from Deno Command')\"")))
+            .build();
+
+        RunContext runContext = TestsUtils.mockRunContext(runContextFactory, commands, ImmutableMap.of());
+        ScriptOutput run = commands.run(runContext);
+
+        assertThat(run.getExitCode(), is(0));
+
+        TestsUtils.awaitLog(logs, log -> log.getMessage() != null && log.getMessage().contains("Hello from Deno Command"));
+        receive.blockLast();
+        assertThat(logs.stream().anyMatch(log -> log.getMessage() != null && log.getMessage().contains("Hello from Deno Command")), is(true));
+    }
+}
---
+++ plugin-script-deno/src/test/java/io/kestra/plugins/scripts/deno/ScriptTest.java
@@ -0,0 +1,57 @@
+package io.kestra.plugins.scripts.deno;
+
+import com.google.common.collect.ImmutableMap;
+import io.kestra.core.junit.annotations.KestraTest;
+import io.kestra.core.models.executions.LogEntry;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.queues.QueueFactoryInterface;
+import io.kestra.core.queues.QueueInterface;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.runners.RunContextFactory;
+import io.kestra.core.utils.IdUtils;
+import io.kestra.core.utils.TestsUtils;
+import io.kestra.plugin.scripts.deno.Script;
+import io.kestra.plugin.scripts.exec.scripts.models.ScriptOutput;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@KestraTest
+public class ScriptTest {
+
+    @Inject
+    RunContextFactory runContextFactory;
+
+    @Inject
+    @Named(QueueFactoryInterface.WORKERTASKLOG_NAMED)
+    private QueueInterface<LogEntry> logQueue;
+
+    @Test
+    void script() throws Exception {
+        List<LogEntry> logs = new ArrayList<>();
+        Flux<LogEntry> receive = TestsUtils.receive(logQueue, l -> logs.add(l.getLeft()));
+
+        Script script = Script.builder()
+            .id(IdUtils.create())
+            .type(Script.class.getName())
+            .script(Property.ofValue("console.log('Hello from deno script.');"))
+            .build();
+
+        RunContext runContext = TestsUtils.mockRunContext(runContextFactory, script, ImmutableMap.of());
+        ScriptOutput run = script.run(runContext);
+
+        assertThat(run.getExitCode(), is(0));
+
+        String expectedLog = "Hello from deno script.";
+        TestsUtils.awaitLog(logs, log -> log.getMessage() != null && log.getMessage().contains(expectedLog));
+        receive.blockLast();
+        assertThat(logs.stream().anyMatch(log -> log.getMessage() != null && log.getMessage().contains(expectedLog)), is(true));
+    }
+}
