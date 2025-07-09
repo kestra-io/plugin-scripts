e4fa244f1d9ffb075c4874dbab67f2feb819294b
Malaydewangan09 <malaydewangan310@gmail.com>
2025-07-09 13:10:17 +0530

    feat(groovy): add groovy Commands and Script tasks

---
+++ plugin-script-groovy/src/test/java/io/kestra/plugin/scripts/groovy/CommandsTest.java
@@ -0,0 +1,54 @@
+package io.kestra.plugin.scripts.groovy;
+
+import com.google.common.collect.ImmutableMap;
+import io.kestra.core.junit.annotations.KestraTest;
+import io.kestra.core.models.executions.LogEntry;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.queues.QueueFactoryInterface;
+import io.kestra.core.queues.QueueInterface;
+import io.kestra.core.runners.RunContext;
+import io.kestra.core.runners.RunContextFactory;
+import io.kestra.core.utils.TestsUtils;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@KestraTest
+public class CommandsTest {
+
+    @Inject
+    RunContextFactory runContextFactory;
+
+    @Inject
+    @Named(QueueFactoryInterface.WORKERTASKLOG_NAMED)
+    private QueueInterface<LogEntry> logQueue;
+
+    @Test
+    void task() throws Exception {
+        List<LogEntry> logs = new ArrayList<>();
+        Flux<LogEntry> receive = TestsUtils.receive(logQueue, l -> logs.add(l.getLeft()));
+
+        var groovyCommands = Commands.builder()
+            .id("groovy-commands")
+            .type(Commands.class.getName())
+            .allowWarning(true)
+            .commands(Property.ofValue(List.of("groovy -e \"println 'I love Kestra!'\"")))
+            .build();
+
+        RunContext runContext = TestsUtils.mockRunContext(runContextFactory, groovyCommands, ImmutableMap.of());
+        var run = groovyCommands.run(runContext);
+
+        assertThat(run.getExitCode(), is(0));
+
+        TestsUtils.awaitLog(logs, log -> log.getMessage() != null && log.getMessage().contains("I love Kestra!"));
+        receive.blockLast();
+        assertThat(logs.stream().anyMatch(log -> log.getMessage() != null && log.getMessage().contains("I love Kestra!")), is(true));
+    }
+}
