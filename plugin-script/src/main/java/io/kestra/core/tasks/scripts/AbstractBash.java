7a26429a96e49d66cface82ee94c2643fad721cc
François Delbrayelle <fdelbrayelle@kestra.io>
2026-03-19 15:27:51 +0100

    fix: correct Property.ofValue/ofExpression type mismatches and rendering issues

--- plugin-script/src/main/java/io/kestra/core/tasks/scripts/AbstractBash.java
+++ plugin-script/src/main/java/io/kestra/core/tasks/scripts/AbstractBash.java
@@ -232,7 +232,7 @@ public abstract class AbstractBash extends Task implements OutputFilesInterface 
         ScriptOutput run = new CommandsWrapper(runContext)
             .withEnv(this.finalEnv(runContext))
             .withTaskRunner(taskRunner)
-            .withCommands(Property.ofValue(JacksonMapper.ofJson().writeValueAsString(commandsArgs)))
+            .withCommands(Property.ofValue(commandsArgs))
             .withOutputFiles(allOutputs)
             .addAdditionalVars(this.additionalVars)
             .run();
